3623c2fa85ec9ae77e9ff0837cbe978b5e7e1b01
Nicola Bonelli <nicola.bonelli@gmail.com>
2016-07-25 18:21:39 +0200

    Minor changes.

--- test/cartesian.cpp
+++ test/cartesian.cpp
@@ -3,20 +3,29 @@
 #include <string>
 #include <iostream>
 
+#include <yats.hpp>
+
+using namespace yats;
+
+auto g = Group("Cartesian")
+
+    .Single("product", []
+    {
+        auto p = std::make_tuple(std::vector<int>{1,2,3},
+                                 std::vector<std::string>{"hello", "world"});
+
+        auto r = pds::cartesian_product(p);
+
+        for(auto &x : r)
+        {
+            std::cout << std::get<0>(x) << ' ' << std::get<1>(x) << std::endl;
+        }
+
+    });
+
 int
 main(int argc, char *argv[])
 {
-    auto p = std::make_tuple(std::vector<int>{1,2,3},
-                             std::vector<std::string>{"hello", "world"});
-
-
-    auto r = pds::cartesian_product(p);
-
-    for(auto &x : r)
-    {
-        std::cout << std::get<0>(x) << ' ' << std::get<1>(x) << std::endl;
-    }
-
-    return 0;
+    return yats::run(argc,argv);
 }
 
