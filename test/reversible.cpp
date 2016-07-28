02368eeb0146d1452ba39a41e5710b6985a2c13f
Nicola Bonelli <nicola.bonelli@gmail.com>
2016-07-28 17:35:35 +0200

    Reversible test updated.

--- test/reversible.cpp
+++ test/reversible.cpp
@@ -1,5 +1,7 @@
 #include "pds/sketch.hpp"
 #include "pds/reversible.hpp"
+#include "pds/range.hpp"
+#include "pds/cartesian.hpp"
 
 #include <iostream>
 
@@ -25,6 +27,36 @@ auto g = Group("Reversible")
                    , 65536
                    , pds::ModularHash<8, std::hash<int>, std::hash<int>> 
                    , pds::ModularHash<8, std::hash<int>, std::hash<int>> > s;
+
+
+        auto r0 = pds::candidates<0>(s, pds::numeric_range<int>(0, 255), 
+                                    std::vector<std::vector<size_t>>{ 
+                                          std::vector<size_t>{1, 8}
+                                        , std::vector<size_t>{1, 8}
+                                    });
+        
+        auto r1 = pds::candidates<1>(s, pds::numeric_range<int>(0, 255), 
+                                    std::vector<std::vector<size_t>>{ 
+                                          std::vector<size_t>{1, 42, 30023}
+                                        , std::vector<size_t>{1, 42, 30023}
+                                    });
+
+        for(auto const &x : r0)
+        {
+            std::cout << "r0: " << x << std::endl;
+        }
+
+        for(auto const &x : r1)
+        {
+            std::cout << "r1: " << x << std::endl;
+        }
+
+        auto res = pds::cartesian_product( 
+                        std::make_tuple(r0, r1)
+                   );
+
+        for(auto & t: res)
+            std::cout << "candidate: " << t << std::endl;
 
     })
     ;
