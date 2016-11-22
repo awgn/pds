e7fd14073042b64daf9273f1509cf574623a0453
Nicola Bonelli <nicola.bonelli@gmail.com>
2016-11-22 10:47:40 +0100

    Hash functions updated.

--- pds/hash.hpp
+++ pds/hash.hpp
@@ -334,6 +334,90 @@ namespace pds {
         }
     };
 
+           
+    struct Wang6
+    {
+     	uint32_t operator()(uint32_t a) const
+		{
+		    a = (a+0x7ed55d16) + (a<<12);
+		    a = (a^0xc761c23c) ^ (a>>19);
+		    a = (a+0x165667b1) + (a<<5);
+		    a = (a+0xd3a2646c) ^ (a<<9);
+		    a = (a+0xfd7046c5) + (a<<3);
+		    a = (a^0xb55a4f09) ^ (a>>16);
+		    return a;
+		}
+    };
+
+    struct Wang7
+    {
+		uint32_t operator()(uint32_t a) const
+		{
+			a -= (a<<6);
+			a ^= (a>>17);
+			a -= (a<<9);
+			a ^= (a<<4);
+			a -= (a<<3);
+			a ^= (a<<10);
+			a ^= (a>>15);
+			return a;
+		}
+    };
+
+
+	struct WangHalfAvalanche
+	{
+		uint32_t operator()(uint32_t a) const 
+		{ 
+			a += ~(a<<15); 
+			a ^= (a>>10); 
+			a += (a<<3); 
+			a ^= (a>>6); 
+			a += ~(a<<11); 
+			a ^= (a>>16); 
+			return a;
+		}
+    };
+	
+
+	struct JavaIntHash
+	{
+		uint32_t operator()(uint32_t h)  const
+		{ 
+			h ^= (h >> 20) ^ (h >> 12); 
+			return h ^ (h >> 7) ^ (h >> 4); 
+		}
+	};
+
+
+	struct HalfAvalanche
+	{
+		uint32_t operator()(uint32_t a) const
+		{
+			a = (a+0x479ab41d) + (a<<8);
+			a = (a^0xe4aa10ce) ^ (a>>5);
+			a = (a+0x9942f0a6) - (a<<14);
+			a = (a^0x5aedd67d) ^ (a>>3);
+			a = (a+0x17bea992) + (a<<7);
+			return a;
+		}
+    };
+
+	struct HalfAvalanche2
+	{
+		uint32_t operator()(uint32_t a) const
+		{
+			a -= (a<<6);
+			a ^= (a>>17);
+			a -= (a<<9);
+			a ^= (a<<4);
+			a -= (a<<3);
+			a ^= (a<<10);
+			a ^= (a>>15);
+			return a;
+		}
+    };
+
 } // namespace pds
 
 
